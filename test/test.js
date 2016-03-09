1ac16a1c04da97a92220c5a7d116896d24807d2c
kgryte <kgryte@gmail.com>
2016-03-09 15:35:02 -0800

    Update tests

--- test/test.js
+++ test/test.js
@@ -21,7 +21,7 @@ var expected2 = require( './fixtures/expected2.json' );
 // TESTS //
 
 tape( 'main export is a function', function test( t ) {
-	t.ok( typeof factorial === 'function', 'main export is a function' );
+	t.equal( typeof factorial, 'function', 'main export is a function' );
 	t.end();
 });
 
@@ -51,7 +51,7 @@ tape( 'if provided `NaN`, the function returns `NaN`', function test( t ) {
 });
 
 tape( 'if `x > 170.6144...`, the function returns positive infinity', function test( t ) {
-	var values = incrspace( 172, 1000, 10.1234 );
+	var values = incrspace( 170.615, 1000, 10.1234 );
 	var v;
 	var i;
 
@@ -76,16 +76,12 @@ tape( 'if `x < -171.56749...`, the function returns positive infinity', function
 
 
 tape( 'the function evaluates the factorial function (positive integers)', function test( t ) {
-	var delta;
-	var tol;
 	var v;
 	var i;
 
 	for ( i = 0; i < data1.length; i++ ) {
 		v = factorial( data1[ i ] );
-		delta = abs( v - expected1[ i ] );
-		tol = 2.75e-12 * Math.max( 1, abs( v ), abs( expected1[ i ] ) );
-		t.ok( delta <= tol, 'within tolerance. x: ' + data1[ i ] + '. Value: ' + v + '. Expected: ' + expected1[ i ] + '. Tolerance: ' + tol + '.' );
+		t.equal( v, expected1[ i ], 'returns '+expected1[i]+' when provided '+v );
 	}
 	t.end();
 });
