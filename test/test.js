21c69dd8354bd965b1dae1f3ae58b60682b8a5f9
kgryte <kgryte@gmail.com>
2016-03-09 16:47:34 -0800

    Replace R test runner with Julia runner

--- test/test.js
+++ test/test.js
@@ -7,15 +7,14 @@ var incrspace = require( 'compute-incrspace' );
 var abs = require( 'math-abs' );
 var PINF = require( 'const-pinf-float64' );
 var NINF = require( 'const-ninf-float64' );
+var EPS = require( 'const-eps-float64' );
 var factorial = require( './../lib' );
 
 
 // FIXTURES //
 
-var data1 = require( './fixtures/data1.json' );
-var expected1 = require( './fixtures/expected1.json' );
-var data2 = require( './fixtures/data2.json' );
-var expected2 = require( './fixtures/expected2.json' );
+var integers = require( './fixtures/integers.json' );
+var decimals = require( './fixtures/decimals.json' );
 
 
 // TESTS //
@@ -76,27 +75,43 @@ tape( 'if `x < -171.56749...`, the function returns positive infinity', function
 
 
 tape( 'the function evaluates the factorial function (positive integers)', function test( t ) {
+	var expected;
+	var delta;
+	var tol;
+	var x;
 	var v;
 	var i;
 
-	for ( i = 0; i < data1.length; i++ ) {
-		v = factorial( data1[ i ] );
-		t.equal( v, expected1[ i ], 'returns '+expected1[i]+' when provided '+v );
+	x = integers.x;
+	expected = integers.expected;
+	for ( i = 0; i < x.length; i++ ) {
+		v = factorial( x[ i ] );
+		if ( v === expected[ i ] ) {
+			t.equal( v, expected[ i ], 'returns '+expected[i]+' when provided '+x[i] );
+		} else {
+			delta = abs( v - expected[ i ] );
+			tol = EPS * Math.max( 1, abs( v ), abs( expected[ i ] ) );
+			t.ok( delta <= tol, 'within tolerance. x: ' + x[ i ] + '. Value: ' + v + '. Expected: ' + expected[ i ] + '. Tolerance: ' + tol + '.' );
+		}
 	}
 	t.end();
 });
 
 tape( 'the function evaluates the factorial function (decimal values)', function test( t ) {
+	var expected;
 	var delta;
 	var tol;
+	var x;
 	var v;
 	var i;
 
-	for ( i = 0; i < data2.length; i++ ) {
-		v = factorial( data2[ i ] );
-		delta = abs( v - expected2[ i ] );
-		tol = 2.75e-12 * Math.max( 1, abs( v ), abs( expected2[ i ] ) );
-		t.ok( delta <= tol, 'within tolerance. x: ' + data2[ i ] + '. Value: ' + v + '. Expected: ' + expected2[ i ] + '. Tolerance: ' + tol + '.' );
+	x = decimals.x;
+	expected = decimals.expected;
+	for ( i = 0; i < x.length; i++ ) {
+		v = factorial( x[ i ] );
+		delta = abs( v - expected[ i ] );
+		tol = 2.75e-12 * Math.max( 1, abs( v ), abs( expected[ i ] ) );
+		t.ok( delta <= tol, 'within tolerance. x: ' + x[ i ] + '. Value: ' + v + '. Expected: ' + expected[ i ] + '. Tolerance: ' + tol + '.' );
 	}
 	t.end();
 });
